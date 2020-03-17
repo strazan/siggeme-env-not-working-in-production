5e92c8fed7aa953058df6ba2f6dd03c371811f09
strazan <sigge@labor.cool>
2020-03-17 23:09:21 +0100

    feat: add dotenv

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 const withSass = require("@zeit/next-sass");
+require("dotenv").config();
 
 /* We need to configure Next.js so that our Sass-Modules are getting
 compiled correctly into CSS when we run our app in the browser */
 
 module.exports = withSass({
-  //   env: {
-  //     STORYBLOK_API_TOKEN: process.env.STORYBLOK_API_TOKEN,
-  //     MY_KEY: process.env.MY_KEY
-  //   },
+  env: {
+    STORYBLOK_API_TOKEN: process.env.STORYBLOK_API_TOKEN,
+    MY_KEY: process.env.MY_KEY
+  },
   cssModules: true,
   cssLoaderOptions: {
     importLoaders: 1,
